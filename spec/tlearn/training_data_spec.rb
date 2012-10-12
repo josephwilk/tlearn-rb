6136a2b6942e5b6ecb1b0787e9d1daa4870b99c8
Joseph Wilk <joe@josephwilk.net>
2012-10-12 14:46:43 +0100

    Check data and output_data work

--- spec/tlearn/training_data_spec.rb
+++ spec/tlearn/training_data_spec.rb
@@ -4,8 +4,34 @@ module TLearn
   describe TrainingData do
     let(:all_zeros){ [0] * 77 }
     let(:all_ones) { [1] * 77 }
+
+    describe "#data" do
+      it "should return all data items" do
+        training_data =  [
+                           [{all_zeros => [1, 0, 0, 0, 0, 0]}, {all_zeros => [1, 0, 0, 0, 0, 0]}],
+                           [{all_ones  => [0, 0, 0, 0, 0, 1]}]
+                         ]
+
+        training_data = TrainingData.new(training_data)
+
+        training_data.data.should == [all_zeros, all_zeros, all_ones]
+      end
+    end
+
+    describe "#output" do
+      it "should return all data items" do
+        training_data =  [
+                           [{all_zeros => [1, 0, 0, 0, 0, 0]}, {all_zeros => [1, 0, 0, 0, 0, 0]}],
+                           [{all_ones  => [0, 0, 0, 0, 0, 1]}]
+                         ]
+
+        training_data = TrainingData.new(training_data)
+
+        training_data.output_data.should == [[1, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1]]
+      end
+    end
     
-    describe '.reset_points' do
+    describe '#reset_points' do
       it "should create the reset points from the training data" do
         training_data =  [
                            [{all_zeros => [1, 0, 0, 0, 0, 0]}, {all_zeros => [1, 0, 0, 0, 0, 0]}],
@@ -17,9 +43,6 @@ module TLearn
         training_data.reset_points.should == [2, 1]
       end
     
-      it "should caculate the training data length" do
-      
-      end
     end
     
     describe '#no_of_data_values' do
@@ -35,7 +58,7 @@ module TLearn
       end
     end
     
-    describe '.no_of_inputs' do
+    describe '#no_of_inputs' do
       it "should return the size of all the training data" do
         training_data =  [
                            [{all_zeros => [1, 0, 0, 0, 0, 0]}, {all_zeros => [1, 0, 0, 0, 0, 0]}],
@@ -48,7 +71,7 @@ module TLearn
       end
     end
 
-    describe '.no_of_ouputs' do
+    describe '#no_of_ouputs' do
       it "should return the size of all the training data" do
         training_data =  [
                            [{all_zeros => [1, 0, 0, 0, 0, 0]}, {all_zeros => [1, 0, 0, 0, 0, 0]}],
