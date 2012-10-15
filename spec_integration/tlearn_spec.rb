5ccd86b1acecdd5e1c2d6531394753eb3408f9bb
Joseph Wilk <joe@josephwilk.net>
2012-10-15 18:36:00 +0100

    Integration tests now tests the training went as expected.

--- spec_integration/tlearn_spec.rb
+++ spec_integration/tlearn_spec.rb
@@ -15,29 +15,31 @@ describe "TLearn" do
   
   let(:out){ StringIO.new }
   
+  let(:number_of_sweeps){ 100 }
+  
   it "should train the tlearn neural network" do
-    training_data = [{[1] * 77 => [1, 0, 0, 0, 0, 0]}, {[1] * 77 => [1, 0, 0, 0, 0, 0]}],
+    training_data = [{[1] * 77 => [1, 0, 0, 0, 0, 0]}],
                     [{[0] * 77 => [0, 0, 0, 0, 0, 1]}]
   
     tlearn = TLearn::Run.new(config, out)
     
-    tlearn.train(training_data, sweeps = 1)
+    tlearn.train(training_data, number_of_sweeps)
   end
   
-  it "should provide a output from the neural network" do
+  it "should used the trained neural network to evaluate the fitness of candidate inputs" do
     test_subject_1 = [0] * 77
     test_subject_2 = [1] * 77
   
     tlearn = TLearn::Run.new(config, out)
   
-    rating_1 = tlearn.fitness(test_subject_1, sweeps = 1)
-    rating_2 = tlearn.fitness(test_subject_2, sweeps = 1)
+    rating_1 = tlearn.fitness(test_subject_1, number_of_sweeps)
+    rating_2 = tlearn.fitness(test_subject_2, number_of_sweeps)
 
-    [[rating_1, test_subject_1], [rating_2, test_subject_2]].each do |ratings, subject|
-      rank = ratings.rindex(ratings.max) + 1
-      puts "rank: #{rank} => #{subject}"
-      p ratings, ""
-    end
+    rank_1 = rating_1.rindex(rating_1.max) + 1
+    rank_2 = rating_2.rindex(rating_2.max) + 1
+
+    rank_1.should == 6
+    rank_2.should == 1
   end
 
 end
