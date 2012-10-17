4dcdedbc2bb33ff2ed0a6d29ce2748c48480599b
Joseph Wilk <joe@josephwilk.net>
2012-10-17 15:44:10 +0100

    Prod at fitness and training with a valid file root.

--- spec_integration/tlearn_ext_spec.rb
+++ spec_integration/tlearn_ext_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
-describe 'The TLearnExt C Extension' do
+describe 'TLearnExt' do
 
   def tlearn_extension
     File.exists?(File.dirname(__FILE__) + '/../lib/tlearn.so') ? 'tlearn.so' : 'tlearn.bundle'
+  end
+
+  def example_file_root
+    File.expand_path(File.dirname(__FILE__) + '/../data')  + '/evaluator'
   end
 
   before(:all) do
@@ -15,12 +19,32 @@ describe 'The TLearnExt C Extension' do
     TLearnExt.fitness({}) rescue{}
   end
 
-  it "should support training" do
-    TLearnExt.train({'sweeps' => '1', 'file_root' => 'test'})
+  describe ".train" do
+    context "with a valid file root" do
+      it "should exit with status 0 (sucess)" do
+        TLearnExt.train({'sweeps' => '1', 'file_root' => example_file_root}).should == 0
+      end
+    end
+
+    context "with an invalid file root" do
+      it "should ext with status 1 (an error)" do
+        TLearnExt.train({'sweeps' => '1', 'file_root' => 'invalid file root'}).should == 1
+      end
+    end
   end
-  
-  it "should support fitness" do
-    TLearnExt.fitness({'sweeps' => '1', 'file_root' => 'test'})
+
+  describe ".fitness" do
+    context "with a valid file root" do
+      it "should exit with status 0 (sucess)" do
+        TLearnExt.fitness({'sweeps' => '1', 'file_root' => example_file_root}).should == 0
+      end
+    end
+
+    context "with an invalid file root" do
+      it "should ext with status 1 (an error)" do
+        TLearnExt.fitness({'sweeps' => '1', 'file_root' => 'invalid file root'}).should == 1
+      end
+    end
   end
   
 end
