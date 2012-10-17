4b190ddf56ae735f0ee700c762b7b0e18a3e18b7
Joseph Wilk <joe@josephwilk.net>
2012-10-17 13:01:02 +0100

    Travis is building a .so file rather than .bundle file.

--- spec_integration/tlearn_ext_spec.rb
+++ spec_integration/tlearn_ext_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe 'The TLearnExt C Extension' do
+
+  def tlearn_extension
+    if File.exists?(File.dirname(__FILE__) + '/../lib/tlearn/tlearn.so')
+      'tlearn.so'
+    else
+      'tlearn.bundle'
+    end
+  end
   
   it "should not segfault" do
-    require 'tlearn.bundle'
+    require tlearn_extension
+
     TLearnExt.run({}) rescue{}
   end
   
