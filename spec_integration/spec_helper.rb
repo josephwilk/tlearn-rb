1e995378d671397d72d7ba60c9d6be859132b2f9
Joseph Wilk <joe@josephwilk.net>
2012-10-21 01:09:44 +0200

    Typos

--- spec_integration/spec_helper.rb
+++ spec_integration/spec_helper.rb
@@ -7,8 +7,8 @@ def example_config
   {:number_of_nodes => 86,
    :output_nodes    => 41..46,
    :linear          => 47..86,
-   :weight_limit.   => 1.00,
-   :selected        => 1..86
+   :weight_limit    => 1.00,
+   :selected        => 1..86,
    :connections     => [{1..81   => 0},
                         {1..40   => :i1..:i77},
                         {41..46  => 1..40},
